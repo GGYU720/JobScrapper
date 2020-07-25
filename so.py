0d3de1e9fbfae7dfe28e550a6a1dabe7d8cae5ba
mghan720 <mghan720@users.noreply.github.com>
2020-07-26 00:32:26 +0900

    StackOverflow Finish

--- so.py
+++ so.py
@@ -20,12 +20,14 @@ def extract_job(html):
     company = company.get_text(strip=True)
     location = location.get_text(strip=True).strip(
         "-").strip("\n")
-    return {"title": title, "company": company, "location": location}
+    job_id = html['data-jobid']
+    return {"title": title, "company": company, "location": location, "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO: Page : {page+1}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
